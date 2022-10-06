4ab5a13f9fe004d9a32c72ff4ce063f3e456cd00
CamilaRCM <101403356+CamilaRCM@users.noreply.github.com>
2022-10-06 16:02:41 -0300

    Add files via upload

---
+++ E2/Conta.java
@@ -0,0 +1,7 @@
+package E2;
+
+public interface Conta {
+    void depositar(double valor);
+    void sacar(double valor);
+    double getSaldo();
+}
---
+++ E2/geradorExtratos.java
@@ -0,0 +1,7 @@
+package E2;
+
+public class geradorExtratos {
+    public void geradorConta(Conta conta){
+        System.out.println("Saldo atual: " + conta.getSaldo());
+    }
+}
